2e0d2c7e833a8603ef78f687775849e5fc07264e
Daniel Marquard <daniel@netidy.com>
2016-12-22 01:28:39 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,20 @@
+# name: Discourse Clyp Onebox
+# about: Adds support for embedding Clyp audio player widgets within Discourse.
+# version: 0.1
+# authors: Daniel Marquard
+# url: https://github.com/critcola/discourse-clyp-onebox
+
+class Onebox::Engine::ClypOnebox
+	include Onebox::Engine
+
+	REGEX = /^https?:\/\/clyp.it\/(\w+)/
+	matches_regexp REGEX
+
+	def id
+		@url.match(REGEX)[1]
+	end
+	
+	def to_html
+		"<iframe width=\"100%\" height=\"160\" src=\"https://clyp.it/#{id}/widget\" frameborder=\"0\"></iframe>"
+	end
+end
